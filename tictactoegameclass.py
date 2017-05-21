2aa1032fccfa509deced5896a7682f875d9c0d5e
Mwarukason <a.mwaruka93@gmail.com>
2017-05-21 20:01:06 +0800

    make x and o win

--- tictactoegameclass.py
+++ tictactoegameclass.py
@@ -1,4 +1,5 @@
 #importing libraries
+import sys
 import os
 os.system("cls") #clear screen for windows
 #os.system("clear") #clear screen for linux/Mac
@@ -7,9 +8,9 @@ os.system("cls") #clear screen for windows
 class Onboard():
     def __init__(self):
         #initialise a list of for values.
-        self.cells = [" ", "X", "O", "X", " ", " ", " ", " ", " ", " "]
+        self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-    #function to dispay objects values
+    #class method dispay to dispay values
     def dispay(self):
         print(" %s | %s | %s " %(self.cells[1], self.cells[2], self.cells[3]))
         print("-----------")
@@ -17,6 +18,100 @@ class Onboard():
         print("-----------")
         print(" %s | %s | %s " %(self.cells[7], self.cells[8], self.cells[9]))
 
-#create an object from Onboard class and dispay the values
+    #update function for players to start play the game
+    def update_box(self, cell_no, player):
+        if self.cells[cell_no] == " ": #prevent value override
+            self.cells[cell_no] = player
+
+    #function for winner X:
+    def winner(self, player):
+        #check for first row:
+        if self.cells[1] == player and self.cells[2] == player and self.cells[3] == player:
+            return True
+        #check for second row:
+        if self.cells[4] == player and self.cells[5] == player and self.cells[6] == player:
+            return True
+        #check for third row:
+        if self.cells[7] == player and self.cells[8] == player and self.cells[9] == player:
+            return True
+        #check for fourth row:
+        if self.cells[1] == player and self.cells[4] == player and self.cells[7] == player:
+            return True
+        #check for five row:
+        if self.cells[2] == player and self.cells[5] == player and self.cells[8] == player:
+            return True
+        #check for sixth row:
+        if self.cells[3] == player and self.cells[6] == player and self.cells[9] == player:
+            return True
+        #check for seventh row:
+        if self.cells[1] == player and self.cells[5] == player and self.cells[9] == player:
+            return True
+        #check for eight row:
+        if self.cells[3] == player and self.cells[5] == player and self.cells[7] == player:
+            return True
+
+        return False
+
+
+    #function for resetting the game:
+    def reset(self):
+        self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+#create instance of Onboad class outside to dispay the values
 board = Onboard()
-board.dispay()
+#board.dispay()
+
+#create function header to print welcome msg
+def print_header():
+    #welcome message
+    print("Welcome to Our Tic Tac Toe Game")
+
+def refresh_screen():
+    #clear the screen again
+    os.system('cls')
+
+    #print the header message
+    print_header()
+
+    #show the board
+    board.dispay()
+
+#create a loop
+while True:
+    refresh_screen()
+
+    #PLAYER x
+    #Gets x input from player to start first with X for python 2.7 use (raw_input)
+    x_choice = int(input("\nX) Please choose number 1 - 9. >"))
+    #create a method to update the takes two args the choice &
+    board.update_box(x_choice, "X")
+
+    #refresh screen
+    refresh_screen()
+
+    #check for winner X and ask your to play again
+    if board.winner("X"):
+        print("\nCongrats X Wins!!")
+        play_again = input("Would you like to play again? (Y/N) > ").upper()
+        if play_again == "Y":
+            board.reset()
+            continue
+        else:
+            break
+
+    #check for winner O and ask your to play again
+    if board.winner("O"):
+        print("\nCongrats O Wins!!")
+        play_again = input("Would you like to play again? (Y/N) > ").upper()
+        if play_again == "Y":
+            board.reset()
+            continue
+        else:
+            break
+
+    #PLAYER O
+    #We need to refresh for value to be stored in memory b4 2nd player player
+    refresh_screen()
+    #Gets O input from player Two to start first with O
+    o_choice = int(input("\nO) Please choose number 1 - 9. >"))
+    board.update_box(o_choice, "O")
